02a4e69af031c971cd316892d42349fac6e4c24f
Aaron Vasquez <aaronvasquez1@gmail.com>
2012-05-02 04:08:26 -0400

    added ability to add image and view it (for debugging)...still looks ugly

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,10 +1,15 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :comment
+  attr_accessible :comment, :image
+  validates_presence_of :comment
+  validates_attachment_presence :image
 
   has_attached_file :image,
                   :styles => { :thumbnail => "100x100#" },
                   :storage => :s3,
                   :s3_credentials => S3_CREDENTIALS
+
+
+
 
 
 end
